e0f18b137830ab262ab8303902f18584701f3938
Hans Husurianto <hans.husurianto@gmail.com>
2024-02-04 17:13:59 -0700

    fix images service

--- pages/index.vue
+++ pages/index.vue
@@ -72,14 +72,34 @@
 <script setup lang="ts">
 import type { Database, Tables } from "~/types/database.types";
 const client = useSupabaseClient<Database>();
-import { MainPhotos } from '@/service/MainPhotos';
-const images = ref();
 
-onMounted(() => {
-	MainPhotos.getImages().then(data => {
-		images.value = data
-	});
-})
+const images = [
+	{
+		itemImageSrc: 'mg/m35_Lockout_ship.png',
+		alt: 'Spawn ship for minigame 35: Lockout',
+		title: 'Spawn ship for Minigame 35: Lockout',
+	},
+	{
+		itemImageSrc: 'mg/m33_Golf.png',
+		alt: 'Minigame 33: Golf',
+		title: 'Minigame 33: Golf',
+	},
+	{
+		itemImageSrc: 'mg/m30_Laser_Tag_lobby.png',
+		alt: 'Lobby for Minigame 30: Laser Tag',
+		title: 'Lobby for Minigame 30: Laser Tag',
+	},
+	{
+		itemImageSrc: 'mg/m28_Mario_Kart_group.png',
+		alt: 'Group photo for Minigame 28: Mario Kart',
+		title: 'Group photo for Minigame 28: Mario Kart',
+	},
+	{
+		itemImageSrc: 'mg/m28_Mario_Kart.png',
+		alt: 'Minigame 28: Mario Kart',
+		title: 'Minigame 28: Mario kart',
+	}
+];
 
 const computedLatestWinners = (n: number) => {
 	let data = latestWinners.value?.filter(winner => winner.placement === n);
